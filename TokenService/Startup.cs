c7ad816b794686318a07fb59643aac0dfb8ce9c6
Stephan Probst <probst_stephan@web.de>
2018-06-09 17:45:58 +0200

    use netcore 2.1 rtm

--- TokenService/Startup.cs
+++ TokenService/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -34,7 +35,7 @@ namespace TokenService
                         };
                     });
 
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
